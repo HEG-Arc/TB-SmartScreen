a13c0d66eeb9d4aee42d997d55c7c3e5c492e47f
Maxime Beck <mbcompte@gmail.com>
2017-05-21 21:56:30 +0200

    Detect Engagement and Distance

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.Kinect;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+using System.Linq;
 
 namespace POC_UserAwareness
 {
@@ -15,8 +17,9 @@ namespace POC_UserAwareness
     public partial class MainWindow : Window
     {
         private const int PIXELS_PER_BYTE = 4;
-        private const int COLOR_FRAME_SIZE_RATIO = 3;
-        private const int HEAD_RECTANGLE_SIZE = 100;
+        private const int COLOR_FRAME_SIZE_RATIO = 2;
+        private const int HEAD_RECTANGLE_SIZE = 200;
+        private const int HEAD_RECT_THICKNESS = 5;
 
         private KinectSensor sensor;
         private ColorFrameReader cfReader;
@@ -24,11 +27,22 @@ namespace POC_UserAwareness
         private WriteableBitmap cfBitmap;
 
         private MultiSourceFrameReader msfr;
+        private int bodyCount;
         private Body[] bodies;
+
+        private FaceFrameSource[] faceFrameSources = null;
+        private FaceFrameReader[] faceFrameReaders = null;
+        private FaceFrameResult[] faceFrameResults = null;
+
+        private int displayWidth;
+        private int displayHeight;
+
+        DetectionResult[] detectionResult;
 
         public MainWindow()
         {
             InitializeComponent();
+            detectionResult = Enumerable.Repeat(DetectionResult.Unknown, 6).ToArray();
             this.Loaded += MainWindow_Loaded;
         }
 
@@ -37,15 +51,58 @@ namespace POC_UserAwareness
             sensor = KinectSensor.GetDefault();
             cfReader = sensor.ColorFrameSource.OpenReader();
             FrameDescription fd = sensor.ColorFrameSource.FrameDescription;
+            displayWidth = fd.Width;
+            displayHeight = fd.Height;
+
             cfDataConverted = new byte[fd.LengthInPixels * PIXELS_PER_BYTE];
-            cfBitmap = new WriteableBitmap(fd.Width, fd.Height, 96, 96, PixelFormats.Pbgra32, null);
+            cfBitmap = new WriteableBitmap(displayWidth, displayHeight, 96, 96, PixelFormats.Pbgra32, null);
 
             image.Source = cfBitmap;
             sensor.Open();
 
-            bodies = new Body[6];
+            bodyCount = sensor.BodyFrameSource.BodyCount;
+            bodies = new Body[bodyCount];
+
+            FaceFrameFeatures faceFrameFeatures = FaceFrameFeatures.FaceEngagement;
+            faceFrameSources = new FaceFrameSource[bodyCount];
+            faceFrameReaders = new FaceFrameReader[bodyCount];
+            for (int i = 0; i < bodyCount; i++)
+            {
+                this.faceFrameSources[i] = new FaceFrameSource(sensor, 0, faceFrameFeatures);
+                this.faceFrameReaders[i] = this.faceFrameSources[i].OpenReader();
+                this.faceFrameReaders[i].FrameArrived += Reader_FaceFrameArrived;
+            }
+
+            this.faceFrameResults = new FaceFrameResult[this.bodyCount];
+
             msfr = sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Body | FrameSourceTypes.Color);
             msfr.MultiSourceFrameArrived += Msfr_MultiSourceFrameArrived;
+        }
+
+        private void Reader_FaceFrameArrived(object sender, FaceFrameArrivedEventArgs e)
+        {
+            using (FaceFrame faceFrame = e.FrameReference.AcquireFrame())
+            {
+                if (faceFrame != null)
+                {
+                    int index = this.GetFaceSourceIndex(faceFrame.FaceFrameSource);
+                    this.faceFrameResults[index] = faceFrame.FaceFrameResult;
+                }
+            }
+        }
+
+        private int GetFaceSourceIndex(FaceFrameSource faceFrameSource)
+        {
+            int index = -1;
+            for (int i = 0; i < this.bodyCount; i++)
+            {
+                if (this.faceFrameSources[i] == faceFrameSource)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
         }
 
         private void Msfr_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
@@ -69,43 +126,23 @@ namespace POC_UserAwareness
 
                                 bodyFrame.GetAndRefreshBodyData(bodies);
                                 bodyCanvas.Children.Clear();
-                                foreach (Body body in bodies)
+                                for (int i = 0; i < bodyCount; i++)
                                 {
-                                    if (body.IsTracked)
+                                    if (this.faceFrameSources[i].IsTrackingIdValid && this.faceFrameResults[i] != null)
                                     {
-                                        lblResult.Text = "";
-                                        foreach (KeyValuePair<JointType, JointOrientation> joint in body.JointOrientations)
-                                        {
-                                            lblResult.Text += joint.Key + " :\n" +
-                                                                    "W : " + Math.Round(joint.Value.Orientation.W, 0) + "\n" +
-                                                                    "X : " + Math.Round(joint.Value.Orientation.X, 0) + "\n" +
-                                                                    "Y : " + Math.Round(joint.Value.Orientation.Y, 0) + "\n" +
-                                                                    "Z : " + Math.Round(joint.Value.Orientation.Z, 0) + "\n\n";
-                                        }
+                                        this.faceFrameResults[i].FaceProperties.TryGetValue(FaceProperty.Engaged, out detectionResult[i]);
+                                    }
+                                    else
+                                    {
+                                        if (this.bodies[i].IsTracked)
+                                            this.faceFrameSources[i].TrackingId = this.bodies[i].TrackingId;
+                                    }
 
-                                        Joint headJoint = body.Joints[JointType.Head];
+                                    if (bodies[i].IsTracked)
+                                    {
+                                        Joint headJoint = bodies[i].Joints[JointType.Head];
                                         if (headJoint.TrackingState == TrackingState.Tracked)
-                                        {
-                                            //JointOrientation orientation;
-                                            //body.JointOrientations.TryGetValue(JointType.Head, out orientation);
-                                            ColorSpacePoint csp = sensor.CoordinateMapper.MapCameraPointToColorSpace(headJoint.Position);
-                                            Rectangle headRect = new Rectangle() { Stroke = Brushes.Red };
-                                            TextBlock headInfos = new TextBlock() { Foreground = Brushes.Red };
-
-                                            headInfos.Text = "X Position : " + headJoint.Position.X + "\n" +
-                                                             "Y Position : " + headJoint.Position.Y + "\n" +
-                                                             "Z Position : " + headJoint.Position.Z + "\n";
-
-                                            headRect.Height = headRect.Width = HEAD_RECTANGLE_SIZE / headJoint.Position.Z;
-
-                                            Canvas.SetLeft(headRect, csp.X / COLOR_FRAME_SIZE_RATIO - headRect.Width / 2);
-                                            Canvas.SetTop(headRect, csp.Y / COLOR_FRAME_SIZE_RATIO - headRect.Height / 2);
-                                            Canvas.SetLeft(headInfos, csp.X / COLOR_FRAME_SIZE_RATIO - headRect.Width / 2);
-                                            Canvas.SetTop(headInfos, csp.Y / COLOR_FRAME_SIZE_RATIO + headRect.Height / 2);
-
-                                            bodyCanvas.Children.Add(headRect);
-                                            bodyCanvas.Children.Add(headInfos);
-                                        }
+                                            this.DrawResults(headJoint, detectionResult[i]);
                                     }
                                 }
                             }
@@ -115,6 +152,44 @@ namespace POC_UserAwareness
             }
             catch
             { }
+        }
+
+        private void DrawResults(Joint headJoint, DetectionResult detectionResult)
+        {
+            ColorSpacePoint csp = sensor.CoordinateMapper.MapCameraPointToColorSpace(headJoint.Position);
+            Rectangle headRect;
+            TextBlock headInfos = new TextBlock() { Foreground = Brushes.Red, FontSize = 25 };
+
+            String strEngagement = "";
+
+            if (detectionResult == DetectionResult.Maybe)
+                strEngagement = "No";
+            else
+                strEngagement = detectionResult.ToString();
+
+            if (detectionResult != DetectionResult.Yes)
+            {
+                headInfos = new TextBlock() { Foreground = Brushes.Red, FontSize = 25 };
+                headRect = new Rectangle() { Stroke = Brushes.Red, StrokeThickness = HEAD_RECT_THICKNESS };
+            }
+            else
+            {
+                headInfos = new TextBlock() { Foreground = Brushes.GreenYellow, FontSize = 25 };
+                headRect = new Rectangle() { Stroke = Brushes.GreenYellow, StrokeThickness = HEAD_RECT_THICKNESS };
+            }
+
+            headInfos.Text = "Distance : " + Math.Round(headJoint.Position.Z, 2) + "m\n" +
+                             "Engagement : " + strEngagement;
+
+            headRect.Height = headRect.Width = HEAD_RECTANGLE_SIZE / headJoint.Position.Z;
+
+            Canvas.SetLeft(headRect, csp.X / COLOR_FRAME_SIZE_RATIO - headRect.Width / 2);
+            Canvas.SetTop(headRect, csp.Y / COLOR_FRAME_SIZE_RATIO - headRect.Height / 2);
+            Canvas.SetLeft(headInfos, csp.X / COLOR_FRAME_SIZE_RATIO - headRect.Width / 2);
+            Canvas.SetTop(headInfos, csp.Y / COLOR_FRAME_SIZE_RATIO + headRect.Height / 2);
+
+            bodyCanvas.Children.Add(headRect);
+            bodyCanvas.Children.Add(headInfos);
         }
     }
 }
